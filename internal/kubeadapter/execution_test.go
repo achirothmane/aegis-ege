1bab23e2cc1a794ae62237dad93b0f16553559ee
Othmane Achir <othy19904@gmail.com>
2026-09-23 15:30:06 +0100

    test: import Kubernetes UID type for execution fixtures

--- internal/kubeadapter/execution_test.go
+++ internal/kubeadapter/execution_test.go
@@ -8,6 +8,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/achirothmane/state-latch/internal/decision"
 )
